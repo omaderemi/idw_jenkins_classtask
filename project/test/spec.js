6450d52c3951a035e5d06ae87ae61e0504a8bae2
omaderemi <32327744+omaderemi@users.noreply.github.com>
2018-05-12 12:12:11 +0200

    Add files via upload

---
+++ project/test/spec.js
@@ -0,0 +1,19 @@
+var now = new Date();
+var d = now.getDate();
+var m = now.getMonth();
+var y = now.getYear();
+
+var dateTime = require('node-datetime');
+var dt = dateTime.create();
+var formatted = dt.format('Y-m-d H:M:S');
+var fs = require("fs");
+
+fs.writeFile("output.txt",formatted ,function (err) {
+
+
+    if (err)
+        return console.log(err);
+    console.log("file saved")
+
+
+});
